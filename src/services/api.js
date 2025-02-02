9d2ce2a659a7a617cd7e997428742b476fe6c020
singhabhishek1 <singh.abhishek@techxsherpa.com>
2025-02-02 23:24:25 +0530

    Json deploy

--- src/services/api.js
+++ src/services/api.js
@@ -5,13 +5,12 @@ import axios from "axios";
 const isProduction = process.env.NODE_ENV === "production";
 
 const api = axios.create({
-  baseURL: isProduction
-    ? process.env.VITE_API_BASE_URL
-    : "http://localhost:3001",
+  baseURL:
+    "https://my-json-server.typicode.com/Abhishekfm/streamify/userGrowth1",
 });
 
-export const getKeyMetrics = () => api.get("api/keyMetrics");
-export const getUserGrowth = () => api.get("api/userGrowth");
-export const getRevenue = () => api.get("api/revenue");
-export const getTopSongs = () => api.get("api/topSongs?_limit=5");
-export const getStreams = (params) => api.get("api/streams", { params });
+export const getKeyMetrics = () => api.get("/keyMetrics");
+export const getUserGrowth = () => api.get("/userGrowth");
+export const getRevenue = () => api.get("/revenue");
+export const getTopSongs = () => api.get("/topSongs?_limit=5");
+export const getStreams = (params) => api.get("/streams", { params });
